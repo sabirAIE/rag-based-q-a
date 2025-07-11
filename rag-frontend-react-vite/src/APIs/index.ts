6ffdb03165cea11f061f82812aa7095b53a4e250
sabiransari <sabir.ansari@idrive.com>
2025-07-11 11:48:05 +0530

    type added for axios interceptor config

--- rag-frontend-react-vite/src/APIs/index.ts
+++ rag-frontend-react-vite/src/APIs/index.ts
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios, { type InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({ baseURL: 'http://localhost:5001' });
 
-api.interceptors.request.use((config) => {
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -14,7 +14,7 @@ api.interceptors.request.use((config) => {
 
 export const DATA_SERVICE = axios.create({ baseURL: 'http://localhost:8001' });
 
-DATA_SERVICE.interceptors.request.use((config) => {
+DATA_SERVICE.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
